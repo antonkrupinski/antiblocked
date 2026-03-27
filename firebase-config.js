b2ccb2f8ecd122659e04e3abe3b09a92b5309218
Anton <antonkrupinski@icloud.com>
2026-03-26 20:57:38 -0400

    fixed

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDk9LjNW1mca7urk5mJr2WNn_RQzPRlDuc",
   authDomain: "anti-blocked.firebaseapp.com",
-  databaseURL: "https://anti-blocked-default-rtdb.firebaseio.com",
   projectId: "anti-blocked",
   storageBucket: "anti-blocked.firebasestorage.app",
   messagingSenderId: "250880897658",
